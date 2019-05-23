1dd018b78f8ecb743d97bbb21a21de2820c21706
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-23 16:01:09 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,15 @@
 function paragraphSelector() {
   return $('p');
 }
+function lastImageSelector() {
+  return $('img:last');
+}
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+function divSelector() {
+  return $('.pics');
+}
+function firstListItem() {
+  return $('#pic-list:first-child');
+}
